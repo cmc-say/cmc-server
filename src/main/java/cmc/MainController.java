ce8b7ddda52a3351fd480ee5bda11a29ce67e820
kkyu0718 <kyuwon0718@gmail.com>
2023-02-13 22:47:17 +0900

    [Feat] 배포 브랜치 변경 main -> master

--- src/main/java/cmc/MainController.java
+++ src/main/java/cmc/MainController.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-
-    @GetMapping("/index.html") // 상태코드 -
-    public String main1() throws Exception{
-        return "main1";
-    }
-
     @GetMapping("/")
     public String main2() throws Exception{
-        return "main2";
+        return "Hello world!";
     }
 
 }

39539259df51d4914a23b74f7d3bf03af1c69fca
kkyu0718 <kyuwon0718@gmail.com>
2023-02-19 04:06:51 +0900

    [#12] UserController 테스트 코드 추가

--- src/test/java/cmc/TestSupport.java
+++ src/test/java/cmc/TestSupport.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,7 +32,9 @@ public class TestSupport {
             final WebApplicationContext context
     ) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 필터 추가
                 .alwaysDo(MockMvcResultHandlers.print())
                 .build();
     }
+
 }
--- src/test/java/cmc/domain/User/controller/UserControllerTest.java
+++ src/test/java/cmc/domain/User/controller/UserControllerTest.java
@@ -1,30 +1,40 @@
 package cmc.domain.User.controller;
 
 import cmc.TestSupport;
-import cmc.domain.User.entity.User;
-import cmc.domain.User.model.SocialType;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.security.Principal;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest extends TestSupport {
 
 //    @DisplayName("user/characters")
 //    @Test
 //    void getCharacters() throws Exception{
-//        mockMvc.perform(MockMvcRequestBuilders.get("api/v1/user/characters"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andDo(print());
 //    }
 
     @DisplayName("유저 회원 탈퇴 테스트")
     @Test
     void deleteUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/user"))
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/v1/user")
+                        .principal(new Principal() {
+                            @Override
+                            public String getName() {
+                                return "1";
+                            }
+                        })
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print());
     }
@@ -36,7 +46,15 @@ class UserControllerTest extends TestSupport {
     @DisplayName("유저 신고 테스트")
     @Test
     void reportUser() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/user/1/report"))
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/user/2/report")
+                        .principal(new Principal() {
+                            @Override
+                            public String getName() {
+                                return "1";
+                            }
+                        })
+                )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print());
     }
@@ -44,7 +62,15 @@ class UserControllerTest extends TestSupport {
     @DisplayName("유저 차단 테스트")
     @Test
     void blockUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/user/1/block"))
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/user/2/block")
+                        .principal(new Principal() {
+                            @Override
+                            public String getName() {
+                                return "1";
+                            }
+                        })
+                )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(print());
     }

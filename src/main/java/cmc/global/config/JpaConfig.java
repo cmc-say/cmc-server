54e558168cfbbb3eafde442e5568b59144bf092e
kkyu0718 <kyuwon0718@gmail.com>
2023-02-19 04:04:45 +0900

    [#12] spring security 적용
    토큰 부분 고정해둔 상태

--- src/main/java/cmc/global/config/JpaConfig.java
+++ src/main/java/cmc/global/config/JpaConfig.java
@@ -5,15 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.Principal;
 import java.util.Optional;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaConfig {
 
-    @Bean
+    @Bean(name = "AuditorAware")
     public AuditorAware<Long> auditorAware() {
-
-        return () -> Optional.of(1L);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        Principal principal = (Principal) authentication.getPrincipal();
+        return () -> Optional.of(Long.parseLong(principal.getName()));
     }
 }
---
+++ src/main/java/cmc/global/jwt/handler/JwtAuthenticationFailureHandler.java
@@ -0,0 +1,35 @@
+package cmc.global.jwt.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFailureHandler implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    )
+            throws IOException {
+        log.info("JwtAuthenticationFailureFilter {}", request);
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.sendError(401, "Unauthorized");
+
+//        ErrorCode errorCode = (ErrorCode) request.getAttribute("exception");
+//        objectMapper.writeValue(response.getWriter(), errorCode);
+    }
+}
+

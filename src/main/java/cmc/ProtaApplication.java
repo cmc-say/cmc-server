46c5c9386cea988e179c8cff53bdda90b4405435
kkyu0718 <kyuwon0718@gmail.com>
2023-02-19 04:06:33 +0900

    [Chore] 어플리케이션 이름 변경

--- src/main/java/cmc/ProtaApplication.java
+++ src/main/java/cmc/ProtaApplication.java
@@ -2,12 +2,13 @@ package cmc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class Application {
+public class ProtaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ProtaApplication.class, args);
 	}
 
 }

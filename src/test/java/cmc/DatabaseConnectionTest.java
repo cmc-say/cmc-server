6865e11a58d51c11a9558d34ab58f5fc2ab48b18
kkyu0718 <kyuwon0718@gmail.com>
2023-02-23 04:55:22 +0900

    [#50] db 연결 테스트 코드

---
+++ src/test/java/cmc/DatabaseConnectionTest.java
@@ -0,0 +1,36 @@
+package cmc;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.aspectj.bridge.MessageUtil.fail;
+
+public class DatabaseConnectionTest extends TestSupport{
+    static {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Disabled("db 연결 테스트")
+    @Test
+    public void testConnection() {
+
+        try(Connection con =
+                    DriverManager.getConnection(
+                            "jdbc:mariadb://localhost:3307/cmc_database",
+                            "root",
+                            "your password!!")){
+            System.out.println(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+}

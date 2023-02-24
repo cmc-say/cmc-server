42b4d172c17feb3f69b201d971b6374e970dd825
kkyu0718 <kyuwon0718@gmail.com>
2023-02-24 09:06:49 +0900

    [#53] schema.sql 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,137 +15,181 @@ DROP TABLE IF EXISTS `word_today` CASCADE;
 DROP TABLE IF EXISTS `block` CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
 
-CREATE TABLE `user` (
-                        `user_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE ,
-                        `social_id`	bigint	NULL,
-                        `social_type`	varchar(10)	NULL,
-                        `refresh_token`	varchar(100)	NULL,
-                        `device_token`	varchar(100)	NULL,
-                        `created_at` varchar(30) NULL,
-                        `modified_at` varchar(30) NULL,
-                    PRIMARY KEY (`user_id`)
+create table avatar
+(
+    avatar_id      bigint auto_increment
+        primary key,
+    user_id        bigint       not null,
+    avatar_name    varchar(12)  null,
+    avatar_message varchar(30)  null,
+    avatar_img     varchar(200) null,
+    created_at     varchar(30)  null,
+    modified_at    varchar(30)  null,
+    constraint avatar_id
+        unique (avatar_id)
 );
 
-CREATE TABLE `avatar` (
-                             `avatar_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                             `user_id`	bigint	NOT NULL,
-                             `avatar_name`	varchar(12)	NULL,
-                             `avatar_message`	varchar(30)	NULL,
-                             `avatar_img`	varchar(200)	NULL,
-                             `created_at` varchar(30) NULL,
-                             `modified_at` varchar(30) NULL,
-                             PRIMARY KEY (`avatar_id`)
+create table avatar_todo
+(
+    avatar_todo_id bigint auto_increment
+        primary key,
+    todo_id        bigint      not null,
+    avatar_id      bigint      not null,
+    created_at     varchar(30) null,
+    modified_at    varchar(30) null,
+    constraint avatar_todo_id
+        unique (avatar_todo_id)
 );
 
-CREATE TABLE `todo` (
-                        `todo_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                        `world_id`	bigint	NOT NULL,
-                        `todo_content`	varchar(50)	NULL,
-                        `created_at` varchar(30) NULL,
-                        `modified_at` varchar(30) NULL,
-                        PRIMARY KEY (`todo_id`)
+create table block
+(
+    block_id         bigint auto_increment
+        primary key,
+    blocking_user_id bigint      null,
+    blocked_user_id  bigint      null,
+    created_at       varchar(30) null,
+    modified_at      varchar(30) null,
+    constraint block_id
+        unique (block_id)
 );
 
-CREATE TABLE `world_avatar` (
-                                   `world_avatar_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                                   `avatar_id`	bigint	NOT NULL,
-                                   `world_id`	bigint	NOT NULL,
-                                   `created_at` varchar(30) NULL,
-                                   `modified_at` varchar(30) NULL,
-                                   PRIMARY KEY (`world_avatar_id`)
+create table hashtag
+(
+    hashtag_id   bigint auto_increment
+        primary key,
+    hashtag_name varchar(20) not null,
+    created_at   varchar(30) null,
+    modified_at  varchar(30) null,
+    constraint hashtag_id
+        unique (hashtag_id),
+    constraint hashtag_name
+        unique (hashtag_name)
 );
 
-CREATE TABLE `hashtag` (
-                           `hashtag_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                           `hashtag_name`	varchar(20)	NOT NULL UNIQUE ,
-                           `created_at` varchar(30) NULL,
-                           `modified_at` varchar(30) NULL,
-                           PRIMARY KEY (`hashtag_id`)
+create table recommended_alarm
+(
+    recommended_alarm_id      bigint auto_increment
+        primary key,
+    recommended_alarm_content varchar(20) null,
+    created_at                varchar(30) null,
+    modified_at               varchar(30) null,
+    constraint recommended_alarm_id
+        unique (recommended_alarm_id)
 );
 
-CREATE TABLE `world_hashtag` (
-                                 `world_hashtag_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                                 `world_id`	bigint	NOT NULL,
-                                 `hashtag_id`	bigint	NOT NULL,
-                                 `created_at` varchar(30) NULL,
-                                 `modified_at` varchar(30) NULL,
-                                 PRIMARY KEY (`world_hashtag_id`)
+create table recommended_todo
+(
+    recommended_todo_id      bigint auto_increment
+        primary key,
+    recommended_world_id     bigint      not null,
+    recommended_todo_content varchar(50) null,
+    created_at               varchar(30) null,
+    modified_at              varchar(30) null,
+    constraint recommended_todo_id
+        unique (recommended_todo_id)
 );
 
-
-CREATE TABLE `world` (
-                         `world_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                         `world_name`	varchar(14)	NULL,
-                         `world_user_limit`	tinyint	NOT NULL,
-                         `world_img`	varchar(200)	NULL,
-                         `world_start_date`	datetime	NULL,
-                         `world_end_date`	datetime	NULL,
-                         `world_notice`	varchar(200)	NULL,
-                         `world_password`	varchar(20)	NULL,
-                         `world_host_user_id`	bigint	NULL,
-                         `created_at` varchar(30) NULL,
-                         `modified_at` varchar(30) NULL,
-                         PRIMARY KEY (`world_id`)
+create table recommended_world
+(
+    recommended_world_id   bigint auto_increment
+        primary key,
+    recommended_world_name varchar(30) null,
+    created_at             varchar(30) null,
+    modified_at            varchar(30) null,
+    constraint recommended_world_id
+        unique (recommended_world_id)
 );
 
-CREATE TABLE `report` (
-                          `report_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                          `report_type`	varchar(20)	NULL,
-                          `reporting_user_id`	bigint	NULL,
-                          `reported_user_id`	bigint	NULL,
-                          `created_at` varchar(30) NULL,
-                          `modified_at` varchar(30) NULL,
-                          PRIMARY KEY (`report_id`)
+create table report
+(
+    report_id         bigint auto_increment
+        primary key,
+    report_type       varchar(20) null,
+    reporting_user_id bigint      null,
+    reported_user_id  bigint      null,
+    created_at        varchar(30) null,
+    modified_at       varchar(30) null,
+    constraint report_id
+        unique (report_id)
 );
 
-CREATE TABLE `avatar_todo` (
-                                  `avatar_todo_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                                  `todo_id`	bigint	NOT NULL,
-                                  `avatar_id`	bigint	NOT NULL,
-                                  `created_at` varchar(30) NULL,
-                                  `modified_at` varchar(30) NULL,
-                                  PRIMARY KEY (`avatar_todo_id`)
+create table todo
+(
+    todo_id      bigint auto_increment
+        primary key,
+    world_id     bigint      not null,
+    todo_content varchar(50) null,
+    created_at   varchar(30) null,
+    modified_at  varchar(30) null,
+    constraint todo_id
+        unique (todo_id)
 );
 
-CREATE TABLE `recommended_alarm` (
-                                    `recommended_alarm_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                                    `recommended_alarm_content`	varchar(20)	NULL,
-                                    `created_at` varchar(30) NULL,
-                                    `modified_at` varchar(30) NULL,
-                                    PRIMARY KEY (`recommended_alarm_id`)
+create table user
+(
+    user_id       bigint auto_increment
+        primary key,
+    social_id     bigint       null,
+    social_type   varchar(10)  null,
+    refresh_token varchar(100) null,
+    device_token  varchar(100) null,
+    created_at    varchar(30)  null,
+    modified_at   varchar(30)  null,
+    constraint user_id
+        unique (user_id)
 );
 
-CREATE TABLE `recommended_world` (
-                                    `recommended_world_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                                    `recommended_world_name`	varchar(30)	NULL,
-                                    `created_at` varchar(30) NULL,
-                                    `modified_at` varchar(30) NULL,
-                                    PRIMARY KEY (`recommended_world_id`)
+create table word_today
+(
+    word_today_id      bigint auto_increment
+        primary key,
+    world_avatar_id    bigint       not null,
+    word_today_content varchar(300) null,
+    created_at         varchar(30)  null,
+    modified_at        varchar(30)  null,
+    constraint word_today_id
+        unique (word_today_id)
 );
 
-CREATE TABLE `recommended_todo` (
-                                   `recommended_todo_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                                   `recommended_world_id`	bigint	NOT NULL,
-                                   `recommended_todo_content`	varchar(50)	NULL,
-                                   `created_at` varchar(30) NULL,
-                                   `modified_at` varchar(30) NULL,
-                                   PRIMARY KEY (`recommended_todo_id`)
+create table world
+(
+    world_id           bigint auto_increment
+        primary key,
+    world_name         varchar(14)  null,
+    world_user_limit   tinyint      not null,
+    world_img          varchar(200) null,
+    world_start_date   datetime     null,
+    world_end_date     datetime     null,
+    world_notice       varchar(200) null,
+    world_password     varchar(20)  null,
+    world_host_user_id bigint       null,
+    created_at         varchar(30)  null,
+    modified_at        varchar(30)  null,
+    constraint world_id
+        unique (world_id)
 );
 
-CREATE TABLE `word_today` (
-                             `word_today_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                             `world_avatar_id`	bigint	NOT NULL,
-                             `word_today_content`	varchar(300)	NULL,
-                             `created_at` varchar(30) NULL,
-                             `modified_at` varchar(30) NULL,
-                             PRIMARY KEY (`word_today_id`)
+create table world_avatar
+(
+    world_avatar_id bigint auto_increment
+        primary key,
+    avatar_id       bigint      not null,
+    world_id        bigint      not null,
+    created_at      varchar(30) null,
+    modified_at     varchar(30) null,
+    constraint world_avatar_id
+        unique (world_avatar_id)
 );
 
-CREATE TABLE `block` (
-                         `block_id`	bigint	NOT NULL AUTO_INCREMENT UNIQUE,
-                         `blocking_user_id`	bigint	NULL,
-                         `blocked_user_id`	bigint	NULL,
-                         `created_at` varchar(30) NULL,
-                         `modified_at` varchar(30) NULL,
-                         PRIMARY KEY (`block_id`)
+create table world_hashtag
+(
+    world_hashtag_id bigint auto_increment
+        primary key,
+    world_id         bigint      not null,
+    hashtag_id       bigint      not null,
+    created_at       varchar(30) null,
+    modified_at      varchar(30) null,
+    constraint world_hashtag_id
+        unique (world_hashtag_id)
 );
+

14e7c2feef6fbe9811d06d1ebdc6cf4c4aac7951
kkyu0718 <kyuwon0718@gmail.com>
2023-02-25 16:24:57 +0900

    [#46] ReportRepository 쿼리로 변경

--- src/main/java/cmc/repository/UserReportRepository.java
+++ src/main/java/cmc/repository/UserReportRepository.java
@@ -2,9 +2,22 @@ package cmc.repository;
 
 import cmc.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserReportRepository extends JpaRepository<Report, Long> {
-    Optional<Report> findReportByReportingUserIdAndReportedUserId(Long reportingUserId, Long reportedUserId);
+    @Query(value = "SELECT r " +
+            "FROM Report r " +
+            "WHERE r.reportedUser.userId = :reportingUserId " +
+            "AND r.reportingUserId = :reportedUserId ")
+    List<Report> findByReportingUserIdAndReportedUser(@Param("reportingUserId") Long reportingUserId, @Param("reportedUserId") Long reportedUserId);
+
+    @Query(value = "SELECT r " +
+            "FROM Report r " +
+            "JOIN r.reportedUser ru " +
+            "WHERE ru.userId = :reportedUserId ")
+    List<Report> findReportByReportedUserId(@Param("reportedUserId") Long reportedUserId);
 }

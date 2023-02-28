663e4ac28cbaf7367633f0461d86846f6683b097
kkyu0718 <kyuwon0718@gmail.com>
2023-03-01 05:32:37 +0900

    [#66] todo -> checkedTodo 매핑 해제
    todo에 매핑을 하면 모든 날짜 모든 캐릭터의 checkedTodo가 다 딸려오기 때문에 해제

--- src/main/java/cmc/domain/Todo.java
+++ src/main/java/cmc/domain/Todo.java
@@ -24,9 +24,6 @@ public class Todo extends BaseEntity {
     @Column(length = 50)
     private String todoContent;
 
-    @OneToMany(targetEntity = CheckedTodo.class, fetch = FetchType.LAZY, mappedBy = "todo", cascade = CascadeType.ALL)
-    private List<CheckedTodo> checkedTodos = new ArrayList<>();
-
     @Builder
     public Todo(
             Long todoId,

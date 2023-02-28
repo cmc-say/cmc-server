08962cd820850ac1b5be81950faacee97d923015
kkyu0718 <kyuwon0718@gmail.com>
2023-02-28 21:54:43 +0900

    [#66] Avatar-AvatarTodo 양방향 매핑

--- src/main/java/cmc/domain/Avatar.java
+++ src/main/java/cmc/domain/Avatar.java
@@ -35,6 +35,9 @@ public class Avatar extends BaseEntity {
     @OneToMany(targetEntity = WorldAvatar.class, fetch = FetchType.LAZY, mappedBy = "avatar", cascade = CascadeType.ALL)
     private List<WorldAvatar> worldAvatars = new ArrayList<>();
 
+    @OneToMany(targetEntity = AvatarTodo.class, fetch = FetchType.LAZY, mappedBy = "avatar", cascade = CascadeType.ALL)
+    private List<AvatarTodo> avatarTodos = new ArrayList<>();
+
     @Builder
     public Avatar(Long avatarId, String avatarName, String avatarMessage, String avatarImg, User user) {
         this.avatarId = avatarId;

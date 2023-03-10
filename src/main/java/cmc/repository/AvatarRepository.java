f77fbf747b409effb4c120662dba70933b355ff7
kkyu0718 <kyuwon0718@gmail.com>
2023-03-10 13:29:14 +0900

    [Fix] 세계관 속 캐릭터 조회 쿼리 수정

--- src/main/java/cmc/repository/AvatarRepository.java
+++ src/main/java/cmc/repository/AvatarRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-    @Query(value = "SELECT a FROM Avatar a JOIN User u WHERE u.userId = :userId")
+    @Query(value = "SELECT a FROM Avatar a JOIN a.user u WHERE u.userId = :userId")
     List<Avatar> findAvatarsByUserId(@Param("userId") Long userId);
 
-    @Query(value = "SELECT a FROM WorldAvatar wa " +
+    @Query(value = "SELECT distinct a FROM WorldAvatar wa " +
             "JOIN wa.avatar a " +
             "JOIN a.user u " +
-            "LEFT JOIN u.blockedUsers bu ON bu.blockedUser.userId = a.user.userId " +
+            "LEFT JOIN u.blockedUsers bu ON bu.blockedUser.userId = u.userId " +
             "WHERE wa.world.worldId = :worldId " +
             "AND (bu.blockingUserId IS NULL OR bu.blockingUserId <> :userId)")
     List<Avatar> getAvatarsByWorldIdWithoutBlockedUser(@Param("userId") Long userId, @Param("worldId") Long worldId);

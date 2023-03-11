5ef37d719e285b6b8fcc00b42468aad0d99f5072
kkyu0718 <kyuwon0718@gmail.com>
2023-03-11 16:04:44 +0900

    [#104] AuthController 컴포넌트 추가

---
+++ src/main/java/cmc/controller/AuthController.java
@@ -0,0 +1,19 @@
+package cmc.controller;
+
+import cmc.service.AuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+@Tag(name = "Auth 컨트롤러")
+public class AuthController {
+
+    private final AuthService authService;
+
+}

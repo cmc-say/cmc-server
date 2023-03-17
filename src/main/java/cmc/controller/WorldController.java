35b33c8b5b427817e85262045a2eed3be85132a8
kkyu0718 <kyuwon0718@gmail.com>
2023-03-17 14:20:05 +0900

    [#123] 해시태그 추가 url 수정

--- src/main/java/cmc/controller/WorldController.java
+++ src/main/java/cmc/controller/WorldController.java
@@ -225,7 +225,7 @@ public class WorldController {
             @ApiResponse(responseCode = "400", description = "존재하지 않는 세계관입니다" +
                     "\t\n방장 유저가 아닌 유저는 접근 권한이 없습니다", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    @PostMapping("/{worldId}/hashtags")
+    @PostMapping("/{worldId}/hashtags/save")
     public ResponseEntity<ResponseDto> updateNewWorldHashtags(
             Principal principal,
             @Parameter(description = "수정할 세계관 id", required = true) @PathVariable("worldId") Long worldId,

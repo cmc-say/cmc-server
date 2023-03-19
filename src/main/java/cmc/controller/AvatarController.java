854d69a3e1a06fd44d652aeb0ee7523394238234
kkyu0718 <kyuwon0718@gmail.com>
2023-03-20 03:14:50 +0900

    [#143] content-type form-data 로 수정

--- src/main/java/cmc/controller/AvatarController.java
+++ src/main/java/cmc/controller/AvatarController.java
@@ -23,6 +23,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,7 +48,7 @@ public class AvatarController {
             @ApiResponse(responseCode = "200", description = "캐릭터 저장 성공"),
             @ApiResponse(responseCode = "400", description = "이미지 업로드 실패")
     })
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ResponseDto> saveAvatar(
             @ModelAttribute SaveAvatarRequestDto req,
             Principal principal) {

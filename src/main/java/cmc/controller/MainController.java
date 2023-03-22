aa027fe0a96273e434e940a7881e0bc12f68c783
kkyu0718 <kyuwon0718@gmail.com>
2023-03-23 02:12:48 +0900

    [#107] 테스트 컨트롤러

--- src/main/java/cmc/controller/MainController.java
+++ src/main/java/cmc/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping
+    @GetMapping("/api")
     public String testService() {
         return "Hello world!";
     }

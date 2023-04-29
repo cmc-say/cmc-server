ee5f7600c8ae4105c67e74b1a0466136b9171aa7
KyuWon  Kim <80209277+kkyu0718@users.noreply.github.com>
2023-04-29 23:55:06 +0900

    Fix/#163 (#164)
    * [Fix] docker pull 추가
    
    * [Fix] redis host 삭제
    
    * [Fix] redis host localhost 로 수정
    
    * [Fix] redis host 수정
    
    * [Fix] host dns에서 ip로 수정
    
    * [Fix] redisson 설정 삭제
    
    * [Fix] DistributeLockAop 주석
    
    * [Fix] redis 주석 처리
    
    * [Fix] redisson 주석
    
    * [Fix] redisson 주석
    
    * [Fix] redisson 주석 추가
    
    * [Fix] user schema social id type 오타 수정
    
    * [Test] 배포 브랜치 복구

--- src/main/java/cmc/utils/redisson/DistributeLockAop.java
+++ src/main/java/cmc/utils/redisson/DistributeLockAop.java
@@ -1,52 +1,52 @@
 package cmc.utils.redisson;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-import org.springframework.stereotype.Component;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.reflect.MethodSignature;
+//import org.redisson.api.RLock;
+//import org.redisson.api.RedissonClient;
+//import org.springframework.stereotype.Component;
+//
+//import java.lang.reflect.Method;
 
-import java.lang.reflect.Method;
-
-@Aspect
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class DistributeLockAop {
-    private static final String REDISSON_KEY_PREFIX = "RLOCK_";
-
-    private final RedissonClient redissonClient;
-    private final AopForTransaction aopForTransaction;
-
-
-    @Around("@annotation(cmc.utils.redisson.DistributeLock)")
-    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        DistributeLock distributeLock = method.getAnnotation(DistributeLock.class);     // (1)
-
-        String key = REDISSON_KEY_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributeLock.key());    // (2)
-
-        RLock rLock = redissonClient.getLock(key);    // (3)
-
-        try {
-            boolean available = rLock.tryLock(distributeLock.waitTime(), distributeLock.leaseTime(), distributeLock.timeUnit());    // (4)
-            if (!available) {
-                return false;
-            }
-
-            log.info("get lock success {}" , key);
-            return aopForTransaction.proceed(joinPoint);    // (5)
-        } catch (Exception e) {
-            log.warn("get lock fail {}" , key);
-            Thread.currentThread().interrupt();
-            throw new InterruptedException();
-        } finally {
-            rLock.unlock();    // (6)
-        }
-    }
-}
+//@Aspect
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class DistributeLockAop {
+//    private static final String REDISSON_KEY_PREFIX = "RLOCK_";
+//
+//    private final RedissonClient redissonClient;
+//    private final AopForTransaction aopForTransaction;
+//
+//
+//    @Around("@annotation(cmc.utils.redisson.DistributeLock)")
+//    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+//        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+//        Method method = signature.getMethod();
+//        DistributeLock distributeLock = method.getAnnotation(DistributeLock.class);     // (1)
+//
+//        String key = REDISSON_KEY_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributeLock.key());    // (2)
+//
+//        RLock rLock = redissonClient.getLock(key);    // (3)
+//
+//        try {
+//            boolean available = rLock.tryLock(distributeLock.waitTime(), distributeLock.leaseTime(), distributeLock.timeUnit());    // (4)
+//            if (!available) {
+//                return false;
+//            }
+//
+//            log.info("get lock success {}" , key);
+//            return aopForTransaction.proceed(joinPoint);    // (5)
+//        } catch (Exception e) {
+//            log.warn("get lock fail {}" , key);
+//            Thread.currentThread().interrupt();
+//            throw new InterruptedException();
+//        } finally {
+//            rLock.unlock();    // (6)
+//        }
+//    }
+//}

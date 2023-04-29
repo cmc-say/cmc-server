c6332505ed8ac8447666a1a4cff0ed908b680bbc
kkyu0718 <kyuwon0718@gmail.com>
2023-04-29 23:31:11 +0900

    [Fix] redis 주석 처리

--- src/main/java/cmc/config/RedissonConfig.java
+++ src/main/java/cmc/config/RedissonConfig.java
@@ -9,21 +9,21 @@ import org.springframework.context.annotation.Configuration;
 
 // redis 설정 보류
 //@Configuration
-public class RedissonConfig {
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    private static final String REDISSON_HOST_PREFIX = "redis://";
-
-    @Bean
-    public RedissonClient redissonClient() {
-        RedissonClient redisson = null;
-        Config config = new Config();
-        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
-        redisson = Redisson.create(config);
-        return redisson;
-    }
-}
+//public class RedissonConfig {
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//
+//    private static final String REDISSON_HOST_PREFIX = "redis://";
+//
+//    @Bean
+//    public RedissonClient redissonClient() {
+//        RedissonClient redisson = null;
+//        Config config = new Config();
+//        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+//        redisson = Redisson.create(config);
+//        return redisson;
+//    }
+//}

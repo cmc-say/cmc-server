d96a16780785058cc21b8ae62381735c144a5090
kkyu0718 <kyuwon0718@gmail.com>
2023-04-29 23:38:21 +0900

    [Fix] redisson 주석

--- src/main/java/cmc/utils/redisson/CustomSpringELParser.java
+++ src/main/java/cmc/utils/redisson/CustomSpringELParser.java
@@ -4,15 +4,15 @@ import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-public class CustomSpringELParser {
-    public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
-        ExpressionParser parser = new SpelExpressionParser();
-        StandardEvaluationContext context = new StandardEvaluationContext();
-
-        for (int i = 0; i < parameterNames.length; i++) {
-            context.setVariable(parameterNames[i], args[i]);
-        }
-
-        return parser.parseExpression(key).getValue(context, Object.class);
-    }
-}
+//public class CustomSpringELParser {
+//    public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
+//        ExpressionParser parser = new SpelExpressionParser();
+//        StandardEvaluationContext context = new StandardEvaluationContext();
+//
+//        for (int i = 0; i < parameterNames.length; i++) {
+//            context.setVariable(parameterNames[i], args[i]);
+//        }
+//
+//        return parser.parseExpression(key).getValue(context, Object.class);
+//    }
+//}
--- src/main/java/cmc/utils/redisson/DistributeLock.java
+++ src/main/java/cmc/utils/redisson/DistributeLock.java
@@ -6,14 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface DistributeLock {
-    String key(); // (1)
-
-    TimeUnit timeUnit() default TimeUnit.MILLISECONDS; // (2)
-
-    long waitTime() default 300L; // (3)
-
-    long leaseTime() default 500L; // (4)
-}
+//@Target(ElementType.METHOD)
+//@Retention(RetentionPolicy.RUNTIME)
+//public @interface DistributeLock {
+//    String key(); // (1)
+//
+//    TimeUnit timeUnit() default TimeUnit.MILLISECONDS; // (2)
+//
+//    long waitTime() default 300L; // (3)
+//
+//    long leaseTime() default 500L; // (4)
+//}

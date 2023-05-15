64bbc5d0b27d57a455962cc557a2964b372b4dee
kkyu0718 <kyuwon0718@gmail.com>
2023-05-16 00:31:12 +0900

    [Feat] 블루그린 배포 세팅

---
+++ script/blue-green.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#ec2 home/ubuntu/ 에 넣는다
+
+IS_GREEN=$(docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
+DEFAULT_CONF="./nginx/conf.d/nginx.conf"
+
+if [ -z $IS_GREEN  ];then # blue라면
+
+  echo "### BLUE => GREEN ###"
+
+  echo "1. get green image"
+  docker-compose pull green # green으로 이미지를 내려받습니다.
+
+  echo "2. green container up"
+  docker-compose up -d green # green 컨테이너 실행
+
+  while [ 1 = 1 ]; do
+  echo "3. green health check..."
+  sleep 3
+
+  REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
+    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+            echo "health check success"
+            break ;
+            fi
+  done;
+
+  echo "4. reload nginx"
+  sudo cp ./nginx/conf.d/nginx.green.conf ./nginx/conf.d/nginx.conf
+  sudo nginx -s rel
+
+  echo "5. blue container down"
+  docker-compose stop blue
+else
+  echo "### GREEN => BLUE ###"
+
+  echo "1. get blue image"
+  docker-compose pull blue
+
+  echo "2. blue container up"
+  docker-compose up -d blue
+
+  while [ 1 = 1 ]; do
+    echo "3. blue health check..."
+    sleep 3
+    REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
+
+    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+      echo "health check success"
+      break ;
+    fi
+  done;
+
+  echo "4. reload nginx"
+  sudo cp ./nginx/conf.d/nginx.blue.conf ./nginx/conf.d/nginx.conf
+  sudo nginx -s reload
+
+  echo "5. green container down"
+  docker-compose stop green
+fi
